f5ca26fee128d37e97883df587cc5cdbdbe2b07d
Your Name <msebas.macias@gmail.com>
2022-07-26 16:13:32 -0400

    e

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,14 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+console.log("si corre");
+var bttn = document.querySelector(".lid-toggle");
+
+function bgColor(){
+    document.querySelector(".page-header").style.backgroundColor="red";
+    console.log("click");
+    document.querySelector(".page-header").innerHTML="New title";
+}
+
+bttn.addEventListener("click",bgColor);
+
